6036f9a396e5754a4179664a36e4a09284b11cbc
ki8yk8 <ki***@gmail.com>
2025-10-18 00:17:52 +0545

    adds welcome screen modal and fixes modal createportal issues

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,7 @@ import useMorseSound from "./components/morse-sound/morse-sound";
 import { english2Morse } from "./utils/convert";
 import Modal from "./ui/modal/modal";
 
-const WELCOME_WORD = "Morse";
+const WELCOME_WORD = "Hello";
 
 function App() {
 	const [show_welcome, set_show_welcome] = useState(true);
@@ -14,30 +14,50 @@ function App() {
 	return (
 		<>
 			<Modal onClose={() => set_show_welcome(false)} isOpen={show_welcome}>
-				Example of a modal
+				<>
+					<h4>
+						Hey Chief <span className="u-font-emoji">👋</span>
+					</h4>
+					<small style={{ marginTop: "0.25rem" }}>
+						Learn, and communicate with morse. Multiple learning, practice, and
+						communication mode with right settings tuned for you.
+					</small>
+					<div
+						style={{
+							marginTop: "1rem",
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							alignItems: "center",
+							gap: "2rem",
+							marginTop: "1rem",
+						}}
+					>
+						<DotDash
+							content={english2Morse(WELCOME_WORD)}
+							colors={[
+								"var(--color-red-pantone)",
+								"var(--color-rich-black)",
+								"transparent",
+							]}
+						/>
+						<button
+							className="u-primary"
+							onClick={() => playMorse(english2Morse(WELCOME_WORD))}
+						>
+							Listen to dits-dahs <span className="u-font-emoji">🔊</span>
+						</button>
+					</div>
+				</>
 			</Modal>
+
 			<header>
 				<h1>
 					Morse<span className="u-color-red-pantone">.</span>
 				</h1>
 			</header>
 
-			<div>
-				<DotDash
-					content={english2Morse(WELCOME_WORD)}
-					colors={[
-						"var(--color-red-pantone)",
-						"var(--color-rich-black)",
-						"transparent",
-					]}
-				/>
-				<button
-					className="u-primary"
-					onClick={() => playMorse(english2Morse(WELCOME_WORD))}
-				>
-					Play Morse Sound
-				</button>
-			</div>
+			<div></div>
 		</>
 	);
 }
--- src/ui/modal/modal.jsx
+++ src/ui/modal/modal.jsx
@@ -1,12 +1,17 @@
+import { createPortal } from "react-dom";
 import { AiOutlineCloseSquare } from "react-icons/ai";
+
+import "./modal.css";
 
 export default function Modal({ onClose, children, isOpen = false }) {
 	if (!isOpen) return;
 
-	return (
+	return createPortal(
 		<div
 			style={{
 				position: "absolute",
+				top: 0,
+				left: 0,
 				height: "100vh",
 				width: "100vw",
 				zIndex: 10,
@@ -16,26 +21,32 @@ export default function Modal({ onClose, children, isOpen = false }) {
 				justifyContent: "center",
 				alignItems: "center",
 			}}
+			className="modal__wrapper"
 		>
 			<section
 				style={{
 					border: "2px solid var(--color-rich-black)",
-					padding: "0.5rem 1rem",
-					maxWidth: "90vw",
-					maxHeight: "90vh",
+					padding: "0.5rem 1rem 1rem 1rem",
 				}}
+				className="modal"
 			>
-				<header style={{
-					display: "flex",
-					justifyContent: "flex-end",
-				}}>
+				<header
+					style={{
+						display: "flex",
+						justifyContent: "flex-end",
+					}}
+				>
 					<button className="u-plain" onClick={onClose}>
-						<AiOutlineCloseSquare size="1.25rem" color="var(--color-red-pantone)"/>
+						<AiOutlineCloseSquare
+							size="1.25rem"
+							color="var(--color-red-pantone)"
+						/>
 					</button>
 				</header>
 
 				<main>{children}</main>
 			</section>
-		</div>
+		</div>,
+		document.getElementById("modal-root")
 	);
 }
