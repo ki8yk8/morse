6188db584d9c3d4f97c720376aa7130e9a5cea36
ki8yk8 <ki***@gmail.com>
2025-10-20 04:14:49 +0545

    adds functionality for spy page but fails

---
+++ readme.md
@@ -0,0 +1,12 @@
+# About the Project
+The project aims to teach and practice Morse Code. If you have ever used war movies or satellite feeds you might here some dihs and dahs. That's morse code. Consider this a new langauge or challenge to learn such that you can communicate with your peers with just some beeps.
+
+## Demo
+TODO
+
+# Tech Stack Used
+1. ReactJS
+2. React-Icons for icons mostly lucide icons,
+3. For colors referred to Coolors.
+4. For sliders used `react-range` package.
+5. Vitest for creating test cases for some of the functions.
--- src/pages/spy-tools/spy-tools.jsx
+++ src/pages/spy-tools/spy-tools.jsx
@@ -1,3 +1,122 @@
+import { useState } from "react";
+import StegCloak from "stegcloak";
+
+import "./style.css";
+
 export default function SpyToolsPage() {
-	return <h2>Secret Page for Spies</h2>
+	const [stage, set_stage] = useState(0);
+	const [data, set_data] = useState({
+		secret: "",
+		password: "",
+		cover: "",
+		spoofed: "",
+	});
+	const stegcloak = new StegCloak();
+
+	const handle_stage_toggle = (target) => {
+		set_data({
+			secret: "",
+			cover: "",
+			spoofed: "",
+		});
+		set_stage(target);
+	};
+
+	const handle_text_on_textarea = (name, event) => {
+		set_data((prev) => ({ ...prev, [name]: event.target.value }));
+	};
+
+	const handle_message_spoof = (event) => {
+		event.preventDefault();
+		console.log(stegcloak.hide(data.secret, data.password, data.cover))
+	};
+
+	const handle_reveal_message = (event) => {
+		event.preventDefault();
+	};
+
+	return (
+		<>
+			<h2>Secret Page for Spies</h2>
+			<p>
+				Cloak your secret message inside a normal looking text using zero-width
+				characters.
+			</p>
+			<small>
+				You can send the message in popular messaging apps like WhatsApp.
+			</small>
+
+			<main className="spy">
+				<div className="spy__button">
+					<button
+						className={`u-plain ${stage === 0 ? "active" : ""}`}
+						onClick={handle_stage_toggle.bind(null, 0)}
+					>
+						Hide the message
+					</button>
+					<button
+						className={`u-plain ${stage === 1 ? "active" : ""}`}
+						onClick={handle_stage_toggle.bind(null, 1)}
+					>
+						Reveal the message
+					</button>
+				</div>
+
+				{stage === 0 && (
+					<section>
+						<form onSubmit={handle_message_spoof}>
+							<div>
+								<small className="u-font-weight-medium">Cover Text</small>
+								<textarea
+									placeholder="Something that you want everyone to see"
+									onChange={handle_text_on_textarea.bind(null, "cover")}
+								/>
+							</div>
+							<div>
+								<small className="u-font-weight-medium">Secret Message</small>
+								<textarea
+									placeholder="The hidden message"
+									onChange={handle_text_on_textarea.bind(null, "secret")}
+								/>
+							</div>
+							<div>
+								<small className="u-font-weight-medium">Pasword</small>
+								<input
+									placeholder="Super secret phrase"
+									onChange={handle_text_on_textarea.bind(null, "password")}
+								/>
+							</div>
+
+							<div>
+								<button type="submit">Get spoofed message</button>
+							</div>
+						</form>
+					</section>
+				)}
+				{stage === 1 && (
+					<section>
+						<form onSubmit={handle_reveal_message}>
+							<div>
+								<small className="u-font-weight-medium">Spoofed Message</small>
+								<textarea
+									placeholder="Message with hidden secret"
+									onChange={handle_text_on_textarea.bind(null, "spoofed")}
+								/>
+							</div>
+							<div>
+								<small className="u-font-weight-medium">Pasword</small>
+								<input
+									placeholder="Super secret phrase"
+									onChange={handle_text_on_textarea.bind(null, "password")}
+								/>
+							</div>
+							<div>
+								<button type="submit">Reveal the Secret</button>
+							</div>
+						</form>
+					</section>
+				)}
+			</main>
+		</>
+	);
 }
