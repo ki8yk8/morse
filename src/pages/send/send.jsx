e1bc3c79d135cb785c6c8cc2bd80578086dee1f1
ki8yk8 <ki***@gmail.com>
2025-10-20 01:44:17 +0545

    adds logic to recognize keypress and show hints

--- src/pages/send/send.jsx
+++ src/pages/send/send.jsx
@@ -1,3 +1,53 @@
+import { useEffect, useState } from "react";
+import useMorseSound from "../../hooks/use-morse-sound/use-morse-sound";
+
+import "./style.css";
+import { FaCircle, FaMinus } from "react-icons/fa";
+
 export default function SendPage() {
-	return <h2>Send Morse Code</h2>;
+	const playMorse = useMorseSound(100);
+	const [playing, set_playing] = useState({
+		dot: false,
+		dash: false,
+	});
+
+	useEffect(() => {
+		window.addEventListener("keydown", handle_key_pressed);
+
+		return () => window.removeEventListener("keydown", handle_key_pressed);
+	}, []);
+
+	const handle_key_pressed = (event) => {
+		if (event.key === ".") {
+			playMorse(".");
+			set_playing((prev) => ({ ...prev, dot: true }));
+
+			setTimeout(() => {
+				set_playing((prev) => ({ ...prev, dot: false }));
+			}, 100);
+		} else if (event.key === "-") {
+			playMorse("-");
+			set_playing((prev) => ({ ...prev, dash: true }));
+
+			setTimeout(() => {
+				set_playing((prev) => ({ ...prev, dash: false }));
+			}, 100);
+		}
+	};
+
+	return (
+		<>
+			<h2>Send Morse Code</h2>
+			<p>Press '.' and '-' on your keyboard to send dot and dash</p>
+
+			<section className="morse_hint">
+				<div className={playing.dot ? "active" : ""}>
+					<FaCircle size="0.5rem" />
+				</div>
+				<div>
+					<FaMinus className={playing.dash ? "active" : ""} />
+				</div>
+			</section>
+		</>
+	);
 }
