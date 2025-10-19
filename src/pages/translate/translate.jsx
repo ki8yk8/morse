ca33a4024bacd8b041ecddb5e3deb1000965d716
ki8yk8 <ki***@gmail.com>
2025-10-20 00:42:11 +0545

    adds basic translation feature

--- src/pages/translate/translate.jsx
+++ src/pages/translate/translate.jsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
+import { english2Morse, morse2English } from "../../utils/convert";
+
+import "./style.css";
+
 export default function TranslatePage() {
+	const [inputs, set_inputs] = useState({
+		english: "Hello World",
+		morse: english2Morse("Hello World"),
+	});
+
+	const handle_text_change = (type, event) => {
+		const input = event.target.value;
+		if (type === "morse") {
+			set_inputs({
+				morse: input,
+				english: morse2English(input),
+			});
+		} else {
+			set_inputs({
+				morse: english2Morse(input),
+				english: input,
+			});
+		}
+	};
+
 	return (
 		<>
 			<h2>Translate</h2>
+			<main style={{ display: "flex" }} className="translate_page">
+				<textarea
+					value={inputs.english}
+					onChange={handle_text_change.bind(null, "english")}
+				/>
+				<textarea
+					value={inputs.morse}
+					onChange={handle_text_change.bind(null, "morse")}
+				/>
+			</main>
 		</>
 	);
 }
