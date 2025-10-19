3583fd9acf8fd274e0c438ec5ca9b4b9ff20af00
ki8yk8 <ki***@gmail.com>
2025-10-20 00:56:24 +0545

    adds and styles translate page with translation logic in two-way

--- src/pages/translate/translate.jsx
+++ src/pages/translate/translate.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import { english2Morse, morse2English } from "../../utils/convert";
 
 import "./style.css";
+import { HiOutlineSpeakerWave } from "react-icons/hi2";
+import useMorseSound from "../../hooks/use-morse-sound/use-morse-sound";
 
 export default function TranslatePage() {
 	const [inputs, set_inputs] = useState({
 		english: "Hello World",
 		morse: english2Morse("Hello World"),
 	});
+	const playMorse = useMorseSound();
 
 	const handle_text_change = (type, event) => {
 		const input = event.target.value;
@@ -28,14 +31,26 @@ export default function TranslatePage() {
 		<>
 			<h2>Translate</h2>
 			<main style={{ display: "flex" }} className="translate_page">
-				<textarea
-					value={inputs.english}
-					onChange={handle_text_change.bind(null, "english")}
-				/>
-				<textarea
-					value={inputs.morse}
-					onChange={handle_text_change.bind(null, "morse")}
-				/>
+				<section>
+					<small>English</small>
+					<textarea
+						value={inputs.english}
+						onChange={handle_text_change.bind(null, "english")}
+					/>
+				</section>
+
+				<section>
+					<small>Morse</small>
+					<textarea
+						value={inputs.morse}
+						onChange={handle_text_change.bind(null, "morse")}
+					/>
+					<footer>
+						<button className="u-hover-no-scale">
+							<HiOutlineSpeakerWave onClick={() => playMorse(inputs.morse)} />
+						</button>
+					</footer>
+				</section>
 			</main>
 		</>
 	);
